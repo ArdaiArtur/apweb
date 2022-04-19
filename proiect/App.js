37cee1b50c767999dcc95d7807dffa6f37cefa59
Artur Ardai <krkerdo@gmail.com>
2022-04-18 14:27:36 -1200

    change

--- proiect/App.js
+++ proiect/App.js
@@ -5,3 +5,17 @@ console.log(popup); //optiune de verificare de existenta a variabilei/constantei
 function TogglePopup(){
 popup.classList.toggle('hidden');
 }
+
+var password = document.getElementById("Parola").value;
+  var confirm_password = document.getElementById("confirm_parola").value;
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+ConfirmPassword.onkeyup = validatePassword;
